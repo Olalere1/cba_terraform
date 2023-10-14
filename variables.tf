b240ab67cd0516a330388e9e9109a6c52278c6e2
Olalere1 <alabi.olalere@yahoo.com>
2023-10-14 15:05:13 +0100

    my first commit terraform 3

--- variables.tf
+++ variables.tf
@@ -6,16 +6,6 @@ variable "instance_type" {
   default = "t2.micro"
 }
 
-variable "instance_ami" {
-  default = "ami-0bb4c991fa89d4b9b"
-}
-
-
-variable "vpc_id" {
-  default = ""
-}
-
-
 variable "key_name" {
   default = "cba_keypair1"
 }
